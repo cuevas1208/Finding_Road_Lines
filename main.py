500ba4f2deb4b65948da80f7696bfd9bf715db90
cuevas1208 <cuevas1208@gmail.com>
2017-04-11 21:27:23 -0500

    Added a user-friendly way to enter the video location through command-line interfaces by using argparse.
    If you would like to process videos, all you have to do is add to the command line an argument with the directory of the videos.Example:
    
    python main.py ../test_video/
    
    The argument of the directory location is not necessary, but if provided the program will find any mp4 videos and will output the processed videos with the roadlines detected. The program also automatically generates help when users give the program invalid arguments.

--- main.py
+++ main.py
@@ -4,46 +4,64 @@
 ## Project: CarND - LaneLines
 ## Desc: Pipeline uses CV techniques like image threshold, GaussianBlur, Canny,
 ## and HoughLinesP.
-## Usage: This project identifies a series of steps that identify and draw the
+## Usage: This project follows a series of steps to identify and draw the
 ## road lane lines from images and video.
 ## This project was part of the CarND program.
 ## Tools learned in class were used to identify lane lines on the road.
 ## Revision: Rev 0000.004 12_11_2016
 #######################################################################################
 #importing useful packages
+import argparse
 import sys
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import cv2
 import math
+import os
+import glob
 from drawLineFunctions import *
 print(sys.version)
 
-# # Pipeline
-# The output you return should be a color image (3 channel) for processing video below
-def pipeline(image):   
+''' Pipeline
+The output you return should be a color image (3 channel) for processing video below
+Input: image - image to be process
+       print_Image - to print pipeline images
+Output: Original image with the the detected road line
+'''
+def pipeline(image, print_Image = False):   
 
     ###################Color and Mask#################################
     #Converts an image from one color space to another
     color_filter = colorTransformation(image)
+    if print_Image:
+        mpimg.imsave("test_results/00color_filter.png", color_filter)
 
     #Mask the image to obtain the image area to work  
     mask_filter = maskImage(image, color_filter)
-    
+
+    #Uncomment the following code if you wish to save the image
+    if print_Image:
+        mpimg.imsave("test_results/01mask_filter.png", mask_filter)
     ###################Gaussian#######################################
     # Define a kernel size and apply Gaussian smoothing
     # ksize.width and ksize.height can differ
     kernel_size = 5  # must be positive and odd
     blur_gray = cv2.GaussianBlur(color_filter,(kernel_size, kernel_size),0)
 
+    #Uncomment the following code if you wish to save the image
+    if print_Image:
+        mpimg.imsave("test_results/02GaussianBlur.png", blur_gray)
     ##################Canny###########################################
     #Define our parameters for Canny and apply
     #Canny finds edges in the input image and marks them in the output map edges
     low_threshold = 50       #made lines think
     high_threshold = 300     #eleminate the background
     edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
-    
+
+    #Uncomment the following code if you wish to save the image
+    if print_Image:
+        mpimg.imsave("test_results/03Edges.png", edges)
     ##################Hough transform#################################
     # Define the Hough transform parameters
     # Make a blank the same size as our image to draw on
@@ -58,46 +76,72 @@ def pipeline(image):
     # Run Hough on edge detected image and lines draw
     hough = hough_lines(edges, rho, theta, threshold, min_line_length, max_line_gap)
     
+    #Uncomment the following code if you wish to save the image
+    if print_Image:
+        mpimg.imsave("test_results/04Hough.png", hough)
     ###################weighted_img###################################
     #Calculates the weighted sum of two arrays
     weighted_img = cv2.addWeighted(hough, alpha=0.8, src2=image, beta=1., gamma=0.)
+
+    #Uncomment the following code if you wish to save the image
+    if print_Image:
+        mpimg.imsave("test_results/05weighted_img.png", weighted_img)
     return (weighted_img)
 
+''' main
+Saves the pipeline output in to test_results directory
+Input: Directory location of the videos to process
+'''
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Remote Arguments')
+    parser.add_argument(
+        'video_location',
+        type=str,
+        nargs='?',
+        default='',
+        help='Path to video folder. This is where the video will be loaded from'
+    )
+    args = parser.parse_args()
 
-### Test on Images
-# Build pipeline to work with the images in the directory "test_images/"  
-import os
-imagesList = os.listdir("test_images/")
-print("Processing ", len(imagesList), " images...")
-for i, imageLocation in (enumerate(imagesList)):
-    #open file
-    image = mpimg.imread("test_images/"+imageLocation)
+    #Create output directory 
+    if not os.path.exists("test_results"):
+        os.makedirs("test_results")
+
+    ### Test on Images
+    # Build pipeline to work with the images in the directory "test_images/"
+    if os.path.exists("test_images/"):
+        imagesList = os.listdir("test_images/")
+        print("Processing ", len(imagesList), " images...")
+        for i, imageLocation in (enumerate(imagesList)):
+            #open file
+            image = mpimg.imread("test_images/"+imageLocation)
+            
+            result = pipeline(image)
+            
+            # Save & Display the results
+            mpimg.imsave("test_results/05"+imageLocation+".png", result)
+            plt.figure()
+            plt.imshow(result)
+            print("Images", i, "saved successfully")
+    else:
+        print("Missing images dirctory test_images")
+
     
-    result = pipeline(image)
-    
-    # Save & Display the results
-    mpimg.imsave("test_results/05"+imageLocation+".png", result)
-    plt.figure()
-    plt.imshow(result)
-    print("Images", i, "saved successfully")
+    ### Test on Videos
+    # Import everything needed to edit/save/watch video clips
+    from moviepy.editor import VideoFileClip
+    from IPython.display import HTML
 
-### Test on Videos
-# Import everything needed to edit/save/watch video clips
-from moviepy.editor import VideoFileClip
-from IPython.display import HTML
+    if args.video_location != '':
+        #capture the filenames under directory
+        files = glob.glob(os.path.join(args.video_location, '*.mp4'))
+        for fileName in files:
+            #Run pipeline for the video
+            file_output = 'test_results/0'+ os.path.basename(fileName)
+            print("Processing video", os.path.basename(fileName))
+            clip1 = VideoFileClip(args.video_location+os.path.basename(fileName))
+            white_clip = clip1.fl_image(pipeline) #NOTE: this function expects color images!!
+            white_clip.write_videofile(file_output, audio=False)
+            print("Video saved successfully")
 
-#Run pipeline with the solid white lane on the right
-white_output = 'test_results/white.mp4'
-clip1 = VideoFileClip("../test_video/solidWhiteRight.mp4")
-white_clip = clip1.fl_image(pipeline) #NOTE: this function expects color images!!
-white_clip.write_videofile(white_output, audio=False)
-
-#Run pipeline with the solid white lane on the left
-yellow_output = 'test_results/yellow.mp4'
-clip2 = VideoFileClip('../test_video/solidYellowLeft.mp4')
-yellow_clip = clip2.fl_image(pipeline)
-yellow_clip.write_videofile(yellow_output, audio=False)
-
-print("Program completed :)")
-
-
+    print("Program completed :)")
